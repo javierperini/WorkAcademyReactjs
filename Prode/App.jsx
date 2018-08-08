f83eb5b8a436674278dc4ba96bb26477aa77f420
Javier Perini <jperini@wormholeit.com>
2018-08-08 12:23:54 -0300

    Hacemos que se pueda ver un partido con goles 0 en un fixture

--- Prode/App.jsx
+++ Prode/App.jsx
@@ -1,151 +1,70 @@
 import React from 'react';
 
 
-class Square extends React.Component {
+class Goles extends React.Component {
+	constructor(props) {
+		super(props);
+		this.goles = 0;
+	}
+	// Lista de partidos
+	render() {   
+	  return (
+	  <div> {this.goles} </div>
+	  )
+	}
+}
+
+class Equipo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.name = props.name;
+	}
+
+	render() {
+		return (
+		<div> 
+		{this.name} <Goles/> 
+		</div>);
+	}
+}
+
+class Partido extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	// Lista de partidos
+	render() {
+		return (
+		 <div>
+			<Equipo name='Local'/>
+			<Equipo name='Visitante'/>
+		 </div>
+		 )
+	}
+}
+
+class Fixture extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	// Lista de partidos
+	render() {
+		return( <div> <Partido/> </div>)
+	}
+}
+
+class Prode extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: '-',
-    };
   }
   
-  
-  render() {
+  render() {   
     return (
-      <button className="square" onClick={this.props.onClick}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  
-   renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-   
-   render() {
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="game">        
+		  <Fixture/>
       </div>
-    );
+    )
   }
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-	  stepNumber: 0,
-    };
-  }
-  
-  onClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (this.calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
-	 stepNumber: history.length,
-    });
-  }
-  
-  calculateWinner(squares) {
-	  const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	  ];
-	  for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-		  return squares[a];
-		}
-	  }
-	  return null;
-   }
-   
-   jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
-
-  render() {
-	const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = this.calculateWinner(current.squares);
-	const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.onClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Game;
+export default Prode;
--- Prode/main.js
+++ Prode/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './App.jsx';
+import Prode from './App.jsx';
 
-ReactDOM.render(<Game />, document.getElementById('app'));
+ReactDOM.render(<Prode />, document.getElementById('app'));
