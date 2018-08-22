bad2644b5ce6a0c327a0b923f99c723fef09e6de
Javier Perini <jperini@wormholeit.com>
2018-08-22 13:00:04 -0300

    Arreglo el guardar resultados

--- Prode/App.jsx
+++ Prode/App.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import partidos from './partidos.json';
 
+ function countProperties(obj) {
+    var count = 0;
+
+    for (var property in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, property)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
 class Goles extends React.Component {
 	constructor(props) {
 		super(props);
@@ -9,12 +22,12 @@ class Goles extends React.Component {
 	}
 	
 	onChange(goles) {
-	  this.setGoles(goles); 	
+	  this.setGoles(goles.target.value); 	
 	}
 	
 	render() {   
 	  return (
-	  <input type='number' className='form-control' onChange={this.onChange.bind(this)} value={this.goles}/>
+	  <input type='number' className='form-control' onChange={this.onChange.bind(this)} />
 	  )
 	}
 }
@@ -23,13 +36,15 @@ class Equipo extends React.Component {
 	constructor(props) {
 		super(props);
 		this.name = props.name;
-		this.goles[this.name] = 0;
 		this.setGolesEquipo = props.setGolesEquipo;
+		this.golesDelEquipo = {};
+		this.golesDelEquipo[this.name] = 0;
 	}
 	
 	setGoles(goles) {
-		this.goles[this.name] = goles; 
-		this.setGolesEquipo(this.goles)
+
+		this.golesDelEquipo[this.name] = goles; 
+		this.setGolesEquipo(this.golesDelEquipo)
 	}
 
 	render() {
@@ -39,7 +54,7 @@ class Equipo extends React.Component {
 			{this.name}
 		  </div>		
 		  <div className='col-md-6'>
-			<Goles setGoles={this.setGoles}/> 
+			<Goles setGoles={(goles)=> this.setGoles(goles) }/> 
 		  </div>
 		</div>);
 	}
@@ -48,11 +63,11 @@ class Equipo extends React.Component {
 class Partido extends React.Component {
 	constructor(props) {
 		super(props);
-		this.saveResult = props.saveResult;
 		this.partido = props.partido;
-		this.complete = false;
-		this.key = getKey(partido);
-		this.result[this.key] = { } 
+		this.key = this.getKey(this.partido);
+		this.result = {};
+		this.result[this.key] = {};
+		this.saveResult = props.saveResult;
 	}
 	
 	getKey(partido) {
@@ -60,8 +75,13 @@ class Partido extends React.Component {
 	}
 	
 	setGolesEquipo(equipo){
-		result = this.result[this.key]
-		this.result[this.key] = Object.assign(equipo, result) 
+		const result = this.result[this.key];
+		this.result[this.key] = Object.assign(equipo, result);
+		if (countProperties(this.result[this.key]) == 2) {
+			console.log(this.saveResult);
+			console.log(this.result);
+			this.saveResult(this.result);
+		}	
 	}
 	
 	render() {
@@ -69,10 +89,10 @@ class Partido extends React.Component {
 		 <div>			
 			 <div className='row alert alert-dark form-group'>
 				<div className='col-md-6'> 				 
-					<Equipo setGolesEquipo={this.setGolesEquipo} name={this.partido.home_team_country}/>
+					<Equipo setGolesEquipo={(equipo) => this.setGolesEquipo(equipo)} name={this.partido.home_team_country}/>
 				</div>
 				<div className='col-md-6'> 				 
-				   <Equipo setGolesEquipo={this.setGolesEquipo} name={this.partido.away_team_country}/>
+				   <Equipo setGolesEquipo={(equipo) => this.setGolesEquipo(equipo)} name={this.partido.away_team_country}/>
 				</div>
 			</div>
 		 </div>
@@ -85,11 +105,18 @@ class Fixture extends React.Component {
 		super(props);
 		this.saveResult = props.saveResult;
 	}
-	// Lista de partidos
+	
+	createPartido(partido) {
+		return(<Partido saveResult={(partido) => this.saveResult(partido)} partido={partido}/> )
+	}
+
 	render() {
 		return( 
 		<div className='container'>
-		  { partidos.map(function(partido){ return(<Partido saveResult={this.saveResult} partido={partido}/> );}) }
+		  {
+		  partidos.map((partido) => this.createPartido(partido)) 
+		  }
+		  <button className='btn btn-danger'> Guardar </button>
 		</div>)
 	}
 }
@@ -100,14 +127,16 @@ class Prode extends React.Component {
 	this.results = [];
   }
   
-  onChange(partido) {
+  saveResult(partido) {
+	console.log('llegue');
 	this.results.push(partido);
+	console.log(this.results);
   }
   
   render() {   
     return (
       <div className="game">        
-		  <Fixture saveResult={this.onChange}/ >
+		  <Fixture saveResult={(partido) => this.saveResult(partido)}/ >
       </div>
     )
   }
